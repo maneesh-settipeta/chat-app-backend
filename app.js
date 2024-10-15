e63620030cf5efd354c5f48caaa42cb9a94f71e4
maneesh-settipeta <maneeshsettipeta@gmail.com>
2024-10-14 21:58:35 -0500

    updated login and signup

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express = require('express');
 const cors = require('cors');
 const { Server } = require('socket.io');
 const { Client } = require('pg');
+const bcrypt = require('bcrypt')
 const app = express();
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
+
 
 const port = 3000;
-
 const http = require('http');
 const server = http.createServer(app);
 
@@ -39,23 +40,24 @@ connection.connect((err) => {
 
 
 
-io.on("connection" ,(socket)=>{
-    console.log("Connected to Socket" , socket.id);
+io.on("connection", (socket) => {
+    console.log("Connected to Socket", socket.id);
 
-    socket.on("joinChat", (user_uuid)=>{
-        console.log(`${user_uuid} Joined`);
+    socket.on("joinChat", (user_uuid) => {
+        console.log(`${user_uuid} Joined 46`);
         socket.join(user_uuid);
     });
 
-    socket.on('sendMessage', async(data)=>{
-        const {logged_in_user_uuid , receiver_uuid ,message} = data;
-        console.log(logged_in_user_uuid , receiver_uuid ,message ,"Received data from client");
+    socket.on('sendMessage', async (data) => {
+        const { logged_in_user_uuid, receiver_uuid, message } = data;
+        console.log(logged_in_user_uuid, receiver_uuid, message, "Received data from client");
         try {
             const query = "INSERT INTO messages (logged_in_user_uuid, receiver_uuid, message) VALUES($1,$2,$3) RETURNING *";
-            const values =[logged_in_user_uuid, receiver_uuid,  message]
-            const saveMessage = await connection.query(query,values);
+            const values = [logged_in_user_uuid, receiver_uuid, message]
+            const saveMessage = await connection.query(query, values);
             console.log(saveMessage.rows[0]);
-            io.to(receiver_uuid).emit('receiveMessage',{logged_in_user_uuid,message});
+            io.to(receiver_uuid).emit('receiveMessage', { logged_in_user_uuid, message });
+            io.to(logged_in_user_uuid).emit('receiveMessage', { logged_in_user_uuid, message });
         } catch (error) {
             console.error('Error saving message to the database:', error);
         }
@@ -93,8 +95,8 @@ app.get("/", async (req, res) => {
     })
 })
 
-app.get('/getUsers', async(req, res)=>{
-  
+app.get('/getUsers', async (req, res) => {
+
     try {
 
         const getUsersQuery = 'SELECT * FROM users';
@@ -105,18 +107,27 @@ app.get('/getUsers', async(req, res)=>{
     }
 })
 
-app.post('/Login', async (req, res) => {
-    const { email } = req.body;
+app.post('/login', async (req, res) => {
+    const { email, userpassword } = req.body;
+    console.log(email, userpassword);
+    
     try {
         const query = 'SELECT * from users WHERE email=$1';
         const values = [email];
         const checkIsUserThere = await connection.query(query, values);
-        if (checkIsUserThere.rows.length > 0) {
-            const user = checkIsUserThere.rows[0];
-            res.status(200).json({ message: "User found", user: user });
+        console.log(checkIsUserThere);
+        
+        const userDetails = checkIsUserThere.rows[0];
+        if (checkIsUserThere.rows.length === 0) {
+            res.status(404).json({ message: "User not found" });
+        }
+        const hashedPassword = userDetails.password
+        const checkPassword = await bcrypt.compare(userpassword, hashedPassword);
+        if (checkPassword) {
+            res.status(202).json({msg:"valid", data:userDetails});
         }
         else {
-            res.status(401).send("Invalid Email Address");
+            res.status(401).json({ msg: "Unauthorized" });
         }
     } catch (error) {
         console.error("Error Creating User", error);
@@ -129,18 +140,29 @@ app.post('/Login', async (req, res) => {
 
 
 
-app.post('/signUp', async (req, res) => {    
+
+app.post('/signUp', async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
+
+    const CheckQuery = 'SELECT * FROM users WHERE email =$1';
+    const values = [email]
+    const checkIfUserEmailExists = await connection.query(CheckQuery, values);
+    if (checkIfUserEmailExists.rows.length !== 0) {
+        console.log("getting in 142");
+        return res.status(409).json({ msg: "User Already Exists" });
+    }
     const useruuid = uuidv4();
     try {
+        const saltRounds = 10;
+        const hashPassword = await bcrypt.hash(password, saltRounds);
         const query = 'INSERT INTO users (first_name, last_name, email, password, user_uuid) VALUES ($1,$2,$3,$4,$5)';
-        const values = [firstName, lastName, email, password, useruuid];
+        const values = [firstName, lastName, email, hashPassword, useruuid];
         await connection.query(query, values);
-        res.status(201).send("User Created Successfully");
+        res.status(201).json({ msg: "User Created Successfully" });
     } catch (error) {
         console.error("Error Creating User", error);
     }
 });
 
 
-app.listen(port);
+server.listen(port);
