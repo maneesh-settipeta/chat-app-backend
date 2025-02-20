c98f52361047706eb73d33cc935c77a3d2d5d9b3
maneesh-settipeta <maneeshsettipeta@gmail.com>
2025-02-20 00:59:05 -0600

    updating on 2-20

--- app.js
+++ app.js
@@ -10,33 +10,33 @@ app.use(cors());
 app.use(express.json());
 
 
-const port = 3000;
+const port = 5000;
 const http = require('http');
 const { log, error } = require('console');
 const server = http.createServer(app);
 
+
 const io = new Server(server, {
     cors: {
-        origin: process.env.origin,
+        origin: "*",
         methods: ["GET", "POST",]
     },
 });
 
 
 const connection = new Client({
-    connectionString: process.env.DATABASE_URL
-    // host: process.env.DB_HOSTNAME,
-    // user: process.env.DB_USERNAME,
-    // port: process.env.DB_PORT,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // ssl: process.env.ssl
+    host: process.env.DB_HOSTNAME,
+    user: process.env.DB_USERNAME,
+    port: process.env.DB_PORT || 5432,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
 })
 
 connection.connect((err) => {
     if (err) {
-        console.log(err);
-        // throw err
+        console.log("Error while connecting to DB", err.stack);
+
         console.error(err)
     }
     console.log(port + "Connected Backend" + process.env.DB_PORT)
@@ -45,12 +45,9 @@ connection.connect((err) => {
 
 
 io.on("connection", (socket) => {
-    console.log("Connected to Socket", socket.id);
     socket.on("joinChat", (receiver_uuid, logged_in_user_uuid) => {
         const sortedUsersId = [receiver_uuid, logged_in_user_uuid].sort().join("_")
-        console.log(`${receiver_uuid} and  ${logged_in_user_uuid} Joined in ${sortedUsersId}`);
         socket.join(sortedUsersId);
-
     });
     socket.on('sendMessage', async (data) => {
         const { logged_in_user_uuid, receiver_uuid, message } = data;
@@ -60,28 +57,26 @@ io.on("connection", (socket) => {
             const query = "INSERT INTO messages (logged_in_user_uuid, receiver_uuid, message, sortedUsersIds) VALUES($1,$2,$3,$4) RETURNING *";
             const values = [logged_in_user_uuid, receiver_uuid, message, sortedusersids]
             const saveMessage = await connection.query(query, values);
-            console.log(saveMessage.rows[0]);
+
             io.to(sortedusersids).emit('receiveMessage', { logged_in_user_uuid, receiver_uuid, message, sortedusersids });
         } catch (error) {
             console.error('Error saving message to the database:', error);
         };
     });
     socket.on("disconnect", () => {
-        console.log("User disconnected", socket.id);
     });
 })
 
 
 app.post('/getMessages', async (req, res) => {
     const { sorteduseruuids } = req.body;
-    console.log(sorteduseruuids, "THIS IS SORTED");
 
     try {
         const query = 'SELECT * FROM messages WHERE sortedusersids=$1';
         const values = [sorteduseruuids];
         const getMessage = await connection.query(query, values);
         res.status(201).json({ data: getMessage.rows });
-        console.log(getMessage.rows);
+
     } catch (error) {
         console.error(error, "Error while fetching");
         res.status(501).json({ msg: "Error while getting messages" });
@@ -108,6 +103,7 @@ app.post('/createSignUp', async (req, res) => {
         res.status(500).json({ msg: "Table created" + error });
     }
 })
+
 app.get("/", async (req, res) => {
     res.json({
         msg: "Getting"
@@ -117,7 +113,6 @@ app.get("/", async (req, res) => {
 app.get('/getUsers', async (req, res) => {
 
     try {
-
         const getUsersQuery = 'SELECT * FROM users';
         const getUsers = await connection.query(getUsersQuery);
         res.json({ data: getUsers.rows });
@@ -128,13 +123,13 @@ app.get('/getUsers', async (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { email, userpassword } = req.body;
-    console.log(email, userpassword);
+
 
     try {
         const query = 'SELECT * from users WHERE email=$1';
         const values = [email];
         const checkIsUserThere = await connection.query(query, values);
-        console.log(checkIsUserThere);
+
 
         const userDetails = checkIsUserThere.rows[0];
         if (checkIsUserThere.rows.length === 0) {
@@ -160,12 +155,11 @@ app.post('/login', async (req, res) => {
 
 app.post('/signUp', async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
-
     const CheckQuery = 'SELECT * FROM users WHERE email =$1';
     const values = [email]
     const checkIfUserEmailExists = await connection.query(CheckQuery, values);
     if (checkIfUserEmailExists.rows.length !== 0) {
-        console.log("getting in 142");
+
         return res.status(409).json({ msg: "User Already Exists" });
     }
     const useruuid = uuidv4();
